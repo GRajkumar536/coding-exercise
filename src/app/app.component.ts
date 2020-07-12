4667ee3f4960ff11688d2d14751a5c531ed34b23
GRajkumar536 <grkumar536@gmail.com>
2020-07-12 20:21:50 +0530

    Issues fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Model } from './Model';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogComponent } from './dialog/dialog.component';
 import { SelectionModel } from '@angular/cdk/collections';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-root',
@@ -16,34 +17,48 @@ export class AppComponent implements OnInit {
   isGrid = true;
   isEdit = false;
   model = new Model();
-  selectedFile: File
   imagePath;
   message: string;
   displayedColumns: string[] = ['checkBos', 'itemName', 'itemPrice', 'itemDescription', 'itemAdditionDate', 'itemImage', 'actions'];
   dataSource = new MatTableDataSource(items);
   selection = new SelectionModel(true, []);
-  data = Object.assign(items);
+  isLocalstorage;
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+  searchText: any;
 
   constructor(private elementRef: ElementRef, public dialog: MatDialog) { }
 
-  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
-
   ngOnInit() {
+    this.isLocalstorage = localStorage.getItem("ISLOCALSTORAGE");
+    if (this.isLocalstorage == "true") {
+      items = JSON.parse(localStorage.getItem("ITEMS"));
+      this.dataSource = new MatTableDataSource(items);
+    }
     this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
 
   ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
     this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = 'rgba(164, 166, 164, 0.38)';
   }
 
   SaveItem(item, form) {
     var count = 0;
-    if (!item.itemId)
-      item.itemId = items.length + 1;
+    this.searchText = null;
+    this.isLocalstorage = localStorage.getItem("ISLOCALSTORAGE");
+    if (this.isLocalstorage == "true")
+      items = JSON.parse(localStorage.getItem("ITEMS"));
+    if (!item.itemId) {
+      var firstItem = items[0];
+      item.itemId = firstItem?.itemId >= 0 ? firstItem.itemId + 1 : 1;
+    }
     if (this.isEdit) {
       items.forEach(record => {
         if (record.itemId == item.itemId) {
           items[count] = item;
+          this.dataSource = new MatTableDataSource(items);
           count = 0;
           this.isEdit = false;
           return
@@ -54,17 +69,24 @@ export class AppComponent implements OnInit {
     else {
       this.isEdit = false;
       items.splice(0, 0, item);
+      localStorage.setItem("ISLOCALSTORAGE", "true");
+      localStorage.setItem("ITEMS", JSON.stringify(items));
+      items = JSON.parse(localStorage.getItem("ITEMS"));
       this.dataSource = new MatTableDataSource(items);
     }
     this.model = new Model();
+    localStorage.setItem("ISLOCALSTORAGE", "true");
+    localStorage.setItem("ITEMS", JSON.stringify(items));
     let dialogRef = this.dialog.open(DialogComponent, { data: { isDelete: false } });
     this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
 
   Edit(item) {
+    this.searchText = null;
     this.isEdit = true;
     this.isGrid = true;
-    this.model = item;
+    this.model = JSON.parse(JSON.stringify(item));
   }
 
   Delete(id) {
@@ -72,15 +94,21 @@ export class AppComponent implements OnInit {
     dialogRef.afterClosed().subscribe(result => {
       if (result == "true") {
         var count = 0;
+        this.searchText = null;
         items.forEach((record, index) => {
           if (record.itemId == id) {
             items.splice(index, 1);
             this.dataSource = new MatTableDataSource(items);
+            localStorage.setItem("ISLOCALSTORAGE", "true");
+            localStorage.setItem("ITEMS", JSON.stringify(items));
+            this.dataSource.paginator = this.paginator;
+            this.dataSource.sort = this.sort;
             return false;
           }
           count = count + 1;
         });
         this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
       }
     });
   }
@@ -102,7 +130,7 @@ export class AppComponent implements OnInit {
   }
 
   filter(filteredText) {
-    this.dataSource.filter = filteredText.trim().toLowerCase();
+    this.dataSource.filter = this.searchText.trim().toLowerCase();
   }
 
   masterToggle() {
@@ -118,21 +146,39 @@ export class AppComponent implements OnInit {
   }
 
   removeSelectedRows() {
-    this.selection.selected.forEach(item => {
-      let index: number = this.data.findIndex(d => d === item);
-      console.log(this.data.findIndex(d => d === item));
-      this.dataSource.data.splice(index, 1);
-      this.dataSource = new MatTableDataSource(this.dataSource.data);
+    this.selection.selected.forEach((selectedItem) => {
+      this.dataSource.data.forEach((item, index) => {
+        if (selectedItem.itemId == item.itemId) {
+          this.dataSource.data.splice(index, 1);
+          this.dataSource = new MatTableDataSource(this.dataSource.data);
+        }
+      });
     });
+    localStorage.setItem("ISLOCALSTORAGE", "true");
+    localStorage.setItem("ITEMS", JSON.stringify(items));
     this.selection = new SelectionModel(true, []);
     this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  isAddItem() {
+    this.isEdit = false;
+    this.model = new Model();
+  }
+
+  isNumber(evt) {
+    evt = (evt) ? evt : window.event;
+    var charCode = (evt.which) ? evt.which : evt.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57))
+      return false;
+    return true;
   }
 }
 
-const items = [
-  { itemId: 1, itemName: "abc", itemImage: "/assets/images/test3.jpg", itemPrice: "100", itemDescription: "Description", itemAdditionDate: "2/16/1997" },
-  { itemId: 2, itemName: "def", itemImage: "/assets/images/unnamed.gif", itemPrice: "200", itemDescription: "Description", itemAdditionDate: "1/15/1997" },
-  { itemId: 3, itemName: "ghi", itemImage: "/assets/images/test2.jpg", itemPrice: "300", itemDescription: "Description", itemAdditionDate: "3/13/1997" },
-  { itemId: 4, itemName: "jkl", itemImage: "/assets/images/test4.jpg", itemPrice: "400", itemDescription: "Description", itemAdditionDate: "3/13/1997" },
-  { itemId: 5, itemName: "mno", itemImage: "/assets/images/test5.png", itemPrice: "500", itemDescription: "Description", itemAdditionDate: "3/13/1997" }
+var items = [
+  { itemId: 5, itemName: "abc", itemImage: "/assets/images/test3.jpg", itemPrice: "100", itemDescription: "Description", itemAdditionDate: "2020-06-30" },
+  { itemId: 4, itemName: "def", itemImage: "/assets/images/unnamed.gif", itemPrice: "200", itemDescription: "Description", itemAdditionDate: "2020-06-30" },
+  { itemId: 3, itemName: "ghi", itemImage: "/assets/images/test2.jpg", itemPrice: "300", itemDescription: "Description", itemAdditionDate: "2020-06-30" },
+  { itemId: 2, itemName: "jkl", itemImage: "/assets/images/test4.jpg", itemPrice: "400", itemDescription: "Description", itemAdditionDate: "2020-06-30" },
+  { itemId: 1, itemName: "mno", itemImage: "/assets/images/test5.png", itemPrice: "500", itemDescription: "Description", itemAdditionDate: "2020-06-30" }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,50 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { FormsModule } from '@angular/forms';
-
-
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button'
 import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
-import {MatSortModule} from '@angular/material/sort';
+import { MatSortModule } from '@angular/material/sort';
 import { MatInputModule } from '@angular/material/input';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatDialogModule } from '@angular/material/dialog';
 import { DialogComponent } from './dialog/dialog.component';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @NgModule({
   declarations: [
-    AppComponent,DialogComponent
+    AppComponent, DialogComponent
   ],
-  entryComponents:[DialogComponent],
+  entryComponents: [DialogComponent],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-
-    FormsModule,
-
-    MatToolbarModule,
-    MatButtonModule,
-    MatCardModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatSortModule,
-    MatInputModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatDialogModule,
-    MatCheckboxModule
+    BrowserModule, AppRoutingModule, BrowserAnimationsModule, FormsModule, MatToolbarModule,
+    MatButtonModule, MatCardModule, MatTableModule, MatPaginatorModule, MatSortModule,
+    MatInputModule, MatDatepickerModule, MatNativeDateModule, MatDialogModule, MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
